de5da7dbc8263fffa9f5b90a58479d68aa456232
CoolGuy <admin@coolguy.pw>
2016-11-22 19:33:25 +0800

    with material ui

--- material-react/src/App.js
+++ material-react/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class App extends Component {
   render() {
@@ -10,6 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React~</h2>
         </div>
+          <div>
+              <RaisedButton label="Default" />
+          </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- material-react/src/index.js
+++ material-react/src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const TheApp = () => (
+    <MuiThemeProvider>
+        <App/>
+    </MuiThemeProvider>
+);
 
 ReactDOM.render(
-  <App />,
+  <TheApp />,
   document.getElementById('root')
 );

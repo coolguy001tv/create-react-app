4cd85543f1e5dc8db9d6d0b083b8641183322d79
CoolGuy <admin@coolguy.pw>
2016-11-27 21:55:15 +0800

    初始化工程

--- material-react/src/components/Layout.js
+++ material-react/src/components/Layout.js
@@ -2,12 +2,25 @@
  * Created by CoolGuy on 2016/11/27.
  */
 import React,{Component} from 'react';
+import {Link} from 'react-router';
+
 export default class Layout extends Component{
 
+    componentDidMount() {
+        console.log("It's ok!");
+    }
     render(){
         return (
             <div>
                 <div>公共头部</div>
+                <div className="left-menu">
+                    <h1>左侧菜单只用于临时展示用，方便各种跳转操作，后面会直接丢掉~</h1>
+                    <ul className="lists">
+                        <li><Link activeClassName="active" to="/login">登录</Link></li>
+                        <li><Link activeClassName="active" to="/reg">注册</Link></li>
+                        <li><Link activeClassName="active" to="/dashboard">dashboard</Link></li>
+                    </ul>
+                </div>
                 <div className="content">
                     {this.props.children}
                 </div>
--- material-react/src/containers/Login/index.js
+++ material-react/src/containers/Login/index.js
@@ -2,11 +2,20 @@
  * Created by CoolGuy on 2016/11/27.
  */
 import React,{Component} from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
 
 var Login = React.createClass({
     render(){
         return (
-            <div>Login</div>
+            <div>
+                <Card>
+
+                </Card>
+                Card--Login
+            </div>
+
         )
 
     }
